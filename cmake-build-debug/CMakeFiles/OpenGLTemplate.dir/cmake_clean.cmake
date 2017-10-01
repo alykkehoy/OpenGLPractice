397cb02ba955b553f44135605729b1bcbec26f42
alykkehoy <alykkeho@purdue.edu>
2017-10-01 10:57:52 -0400

    init commit

---
+++ cmake-build-debug/CMakeFiles/OpenGLTemplate.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGLTemplate.dir/main.cpp.obj"
+  "CMakeFiles/OpenGLTemplate.dir/glad/src/glad.c.obj"
+  "OpenGLTemplate.pdb"
+  "OpenGLTemplate.exe"
+  "OpenGLTemplate.exe.manifest"
+  "libOpenGLTemplate.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/OpenGLTemplate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Talking-Head.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Talking-Head.
+# This may be replaced when dependencies are built.
